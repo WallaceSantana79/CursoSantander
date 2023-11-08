798b4b5b73d0130e32dcde9f73cdfc69deb15db4
Bowie <marketing@letscode.com.br>
2023-11-08 11:39:43 -0300

    Mudanças

--- aulaboolean.java
+++ aulaboolean.java
@@ -2,7 +2,7 @@ public class aulaboolean {
     public static void main(String[] args) {
         boolean fimDeSemana = true;
         boolean fazendoSol = true;
-        boolean vamosAPraia = fimDeSemana && fazendoSol;
+        boolean vamosAPraia = fimDeSemana && fazendoSol; // Utilizando || para se designar a "ou" o que foi usado foi o && que se designa a "e"
         System.out.println(vamosAPraia);
 
         String mensagem = fimDeSemana ? "É fim de semana" : "Não é fim de semana";
